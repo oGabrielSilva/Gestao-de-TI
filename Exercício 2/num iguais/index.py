2911d8fed00ee311fe79147aa4b394256363a823
Gabriel Henrique da Silva <gabriel04gh1.gh@gmail.com>
2021-08-09 16:28:55 -0300

    Subindo ao Github

--- Exercício 2/num iguais/index.py
+++ Exercício 2/num iguais/index.py
@@ -1,4 +1,4 @@
 while True: 
 	nums = [int(input('Digite o primeiro número:\n')), int(input('Digite o segundo número:\n'))]
-	if nums[0] == nums[1]: print('---------------------------\n>> Números iguais.\n---------------------------')
-	else: print('---------------------------\n>> Números diferentes.\n---------------------------')
+	if nums[0] == nums[1]: print('Números iguais.\n')
+	else: print('Números diferentes.\n')
