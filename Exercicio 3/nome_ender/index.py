68d3959f449b5d2fc5a4bb9646e1f119ccb40d8a
Gabriel Henrique da Silva <gabriel04gh1.gh@gmail.com>
2021-08-23 17:40:37 -0300

    Ajuste em nome_ender/index.py de identação.

--- Exercicio 3/nome_ender/index.py
+++ Exercicio 3/nome_ender/index.py
@@ -1,14 +1,14 @@
 def main(): 
     user_name = input('Digite seu nome: ');
     user_address = input('Digite seu endereço: ');
-    if(def_counter(user_name, 20)): 
-        return print(f'\nOpss... {user_name} possui {len(user_name)}. Limite de 20.');   
+    if(def_counter(user_name, 20)):
+        return print(f'\nOpss... {user_name} possui {len(user_name)}. Limite de 20.');
     elif(def_counter(user_address, 30)): 
-        return print(f'\nOpss... {user_address} possui {len(user_address)}. Limite de 30');
+        return print(f'\nOpss... {user_address} possui {len(user_address)}. Limite de 30.');
     print(f'\nNome: {user_name};\nEndereço: {user_address}.');
 
 def def_counter(counter, val): 
-    if(len(counter) > val): return True; 
+    if(len(counter) > val): return True;
     else: return False;
 
 main()
