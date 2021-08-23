da9406e72d81a6abede52047dbfb055376988ef9
Gabriel Henrique da Silva <gabriel04gh1.gh@gmail.com>
2021-08-23 17:38:04 -0300

    Exercício 3

---
+++ Exercicio 3/http/index.py
@@ -0,0 +1,5 @@
+def main():
+    user_url = input('Digite a url (ex: https://github.com):\n');
+    if(url.startswith('https://')): print('Site seguro.');
+    else: print('Site inseguro.');
+main()
